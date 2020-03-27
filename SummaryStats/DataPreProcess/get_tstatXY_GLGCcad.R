f0b0a3836bcdd62290d66f2a761aadd2979bb232
LizaDarrous <30754626+LizaDarrous@users.noreply.github.com>
2020-03-27 20:12:23 +0100

    Update get_tstatXY_GLGCcad.R
    Updated the column naming and the effects for swapped alleles

--- SummaryStats/DataPreProcess/get_tstatXY_GLGCcad.R
+++ SummaryStats/DataPreProcess/get_tstatXY_GLGCcad.R
@@ -6,33 +6,46 @@ library(DescTools)
 
 ## Working directory should be a unique directory to the trait pair to be compared, not the same one as where
 # the original data files are held.
-fileX = "~/data/CAD_uniq.tsv"  # CARDIo trait
-fileY = "~/data/LDL_uniq.tsv"  # GLGC trait
+fX = fread(cmd = " zcat < ~/data/CAD_META.gz.txt.gz", header=TRUE)      # CARDIo trait
+fY = fread(cmd = " zcat < ~/data/jointGwasMc_LDL.txt.gz", header=TRUE)  # GLGC trait
 
-fX = fread(fileX) #fread(cmd = " zcat < ~/data/CAD_META.gz.txt.gz", header=TRUE)
-fY = fread(fileY) #fread(cmd = " zcat < ~/data/jointGwasMc_LDL.txt.gz", header=TRUE)
+## Update column to have consistent comparison
+fX$rsid=fX$oldID
 
-#VARinfo = fread(cmd = " zcat < ~/data/variants.tsv.bgz", sep="\t", header=TRUE)
-#fY = inner_join(fY, VARinfo[,c(1,4,5)])  #Joining, by = "variant" , 1,4,6 = variant, ref, alt - done in get_tstatXY_UKBB.R, if not then uncomment
-
-#Get overlapping SNPS between the two traits 
-comm=intersect(fY$rsid,fX$oldID)
-fX_sub=fX[fX$oldID %in% comm,]
+## Get overlapping SNPs between the two traits 
+comm=intersect(fY$rsid,fX$rsid)
+fX_sub=fX[fX$rsid %in% comm,]
 fY_sub=fY[fY$rsid %in% comm,]
 
-fX1=fX_sub[order(fX_sub$oldID),] 
-fY1=fY_sub[order(fY_sub$rsid),] 
+## Check for duplication and delete if found
+length(which(duplicated(fX_sub$rsid)==TRUE)) # 9 dups
+length(which(duplicated(fY_sub$rsid)==TRUE))
 
-## Add info to GLGC and Cardio files to match those of UKBB
-#Get tsat (standerdised effects) and average sample size to be used as Ny
-fX1$tstat=fX1$Effect/fX1$StdErr
+dups=which(duplicated(fX_sub$rsid)==TRUE)
+fX_sub2 = fX_sub[-c(dups,dups2),]
+
+## Second set of overlapping SNPs between the two traits after duplicate deletion
+comm2=intersect(fY_sub$rsid,fX_sub2$rsid)
+fY_sub2=fY_sub[fY_sub$rsid %in% comm2,]
+
+## Order SNPs
+fX1=fX_sub2[order(fX_sub2$rsid),] 
+fY1=fY_sub2[order(fY_sub2$rsid),] 
+#all(fX1$rsid==fY1$rsid) #check all to be true
+
+
+## Add info to files to match those of UKBB (check columns)
+#Get tsat (standerdised effects) and average sample size to be used as N in LHC-MR
+fX1$beta=fX1$Effect
+fX1$SE=fX1$StdErr
+fX1$tstat=fX1$beta/fX1$SE
 fX1$n_complete_samples=380831 #Effective sample size calculated from (4*N_controls*N_cases)/N_control+N_cases
 fX1$A1=toupper(fX1$Allele1)
 fX1$A2=toupper(fX1$Allele2)
-fX1$rsid=fX1$oldID #In analysis file, rsid is needed to match SNPs to LD file
 
-fY1$tstat=fY1$beta/fY1$se
-fY1$n_complete_samples=mean(fY1$N)
+fY1$SE=fY1$se
+fY1$tstat=fY1$beta/fY1$SE
+fY1$n_complete_samples=fY1$N
 fY1$A1=toupper(fY1$A1)
 fY1$A2=toupper(fY1$A2)
 
@@ -43,9 +56,16 @@ swapped = which(fY1$A1==fX1$A2 &
                   fY1$A2==fX1$A1)
 #Correct the effect of swapped alleles
 fX1[swapped,'tstat']=fX1[swapped,'tstat']*-1
+fX1[swapped,'beta']=fX1[swapped,'beta']*-1
+temp_A1=fX1$A1
+fX1[swapped,'A1']=fX1[swapped,'A2']
+fX1[swapped,'A2']=temp_A1[swapped]
 
 fX2=fX1[c(aligned,swapped),]
 fY2=fY1[c(aligned,swapped),]
+
+## test swapping, should equal 0
+length(which(fY2$A1==fX2$A2 & fY2$A2==fX2$A1))
 
 #These files will be used as input in LHC Analysis
 fwrite(fX2, "CAD_uniq.tsv", sep="\t")
