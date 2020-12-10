f47cea843ba09b658e588986373bc53bf4484073
LizaDarrous <30754626+LizaDarrous@users.noreply.github.com>
2020-12-10 18:29:40 +0100

    Update and rename get_tstatXY_UKBB.R to get_tstatX_UKBB.R
    Added the tstat column, and further explained how this can be used for multiple traits coming from the same cohort, that can then easily be paired up for analysis.

--- SummaryStats/DataPreProcess/get_tstatX_UKBB.R
+++ SummaryStats/DataPreProcess/get_tstatX_UKBB.R
@@ -1,4 +1,6 @@
 ### Script to obtain the SNPs for which we had LD and regression weight calculation from UKBiobank summary statistics only!
+### This is best used if all the trait pairs to be run come from the same cohort, then this script can be used for each individual trait
+### and the resulting files can be paired up for analysis. 
 library(data.table)
 library(dplyr)
 library(DescTools)
@@ -19,14 +21,18 @@ selF = which(info>.99 & abs(mafUK10K-mafUKBB)<(2*sqrt(1/4e3+1/360e3)) & mafUKBB>
 Xfile = fread(cmd = " zcat < 21001_irnt.gwas.imputed_v3.both_sexes.tsv.bgz", header=TRUE) ## Summary statistics file, shoudl be repeated for each trait.
 VARinfo = fread(cmd = " zcat < variants.tsv.bgz", sep="\t", header=TRUE) ## Variant info file from UKBB GWAS Imputed v3 - File Manifest Release 20180731
 
+## Rename/add columns
 Xfile1 = inner_join(Xfile, VARinfo[,c(1:6)]) #Joining, by = "variant"
-LDrs = rs[selF] ## Selected SNPs above
+Xfile1$tstat=Xfile1$beta/Xfile1$se
 
+## Select SNPs found in the LD file
+LDrs = rs[selF] 
 Xfile2=Xfile1[Xfile1$rsid %in% LDrs,]
+
+## Remove duplicates by selecting the SNPs with a MAF closer to the UK10K MAF found in the LD file
 dups=which(duplicated(Xfile2$rsid)==TRUE) #Some duplication arrises due to joining with VARinfo file that has multiple entery for the same SNP
 dups_index = NA
 
-## Remove all SNP duplicates by checking which of each set is closest to the MAF in the LD file
 for(i in dups){
   tab=Xfile2[Xfile2$rsid==Xfile2$rsid[i],]
   ld_maf=LDfile[which(LDfile$rs==Xfile2$rsid[i]),5] #UKBB_AF
@@ -35,9 +41,9 @@ for(i in dups){
 
 ## Filter out unwanted duplicate SNP rows
 Xfile3=Xfile2[!rownames(Xfile2) %in% dups_index, ]
-# Confirm unique SNPs
-length(which(duplicated(Xfile3$rsid)==TRUE))
+# Order and confirm unique SNPs
+Xfile4=Xfile3[order(Xfile3$rsid),] 
+length(which(duplicated(Xfile4$rsid)==TRUE))
+
 ## Write trait file to be used on LHC-MR analysis
-fwrite(Xfile3, "BMI_uniq.tsv", sep="\t")
-
-
+fwrite(Xfile4, "BMI_uniq.tsv", sep="\t")
